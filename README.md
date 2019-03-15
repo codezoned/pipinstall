50976fa443479ded7b90de7a85fe1fc6979d11c5
Aniket Mishra <38854722+Aniket-Mishra@users.noreply.github.com>
2019-03-16 01:53:35 +0530

    Script changes
    Program.py can download libraries from any available domain from command line
    structure fixes
    readme update

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # pipinstall
-A Python library to install required libraries in every domain.
+
+A python library that downloads all required python libraries in any domain specified. 
+
+We don't have to install the required libraries one by one now.
+
+
+
+### Domains available:
+
+1. cryptography
+
+2. games
+
+3. gui
+
+4. img_n_vid
+
+5. library_management
+
+6. ml
+
+7. networking
+
+8. web_dev
+
+### Run command
+
+Go to pipinstall directory
+
+open command line/powershell/terminal at that directory 
+
+run:
+
+    python program.py domain_name
+
+    *domain_name must be one of the above mentioned domains*
+
+   
+
+~~**pip install pipinstall**~~
+
+~~This command will install the library to the machine.~~
+
+~~**pipinstall domain_name**~~
+
+~~This command will install all the libraries in the domain to the machine.~~
--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
  
 setuptools.setup(
-    #Here is the module name.
+    #Module name
     name="ppipinstall",
  
-    #version of the module
+    #Module version
     version="0.0.1",
  
     #Name of Author
     author="Aniket Mishra",
  
-    #your Email address
+    #Author's email address
     author_email="mishra1997aniket@gmail.com",
  
     #Small Description about module
@@ -24,7 +24,7 @@ setuptools.setup(
     #Specifying that we are using markdown file for description
     long_description_content_type="text/markdown",
  
-    #Any link to reach this module, if you have any webpage or github profile
+    #link to reach this module, i.e. any webpage or github profile
     url="https://github.com/codezoned/pipinstall",
     packages=setuptools.find_packages(),
  
